72a469ade9dc8dec3f8122d8767c35c88944c76b
Daniel <daniel.vliegenthart@exotix.com>
2019-05-17 16:29:37 +0100

    Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-require('dotenv').config();
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log("TEST");
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
