d71ac9e786c27eae572585c629060bbd63fff586
Daniel <daniel.vliegenthart@exotix.com>
2019-05-20 13:13:08 +0100

    Add mutations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from "uuid/v4";
 
 const app = express();
 
@@ -8,27 +9,138 @@ app.use(cors());
 
 const schema = gql`
   type Query {
+    users: [User!]
+    user(id: ID!): User
     me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
+  }
+
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
+    updateMessage(id: ID!, text: String!): Message!
   }
 
   type User {
+    id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+    messageIds: [1],
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+    messageIds: [2],
+  }
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1"
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2"
+  }
+};
+
 const resolvers = {
   Query: {
-    me: () => {
-      return {
-        username: 'Robin Wieruch',
-      };
+    users: () => {
+      return Object.values(users);
     },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: (parent, args, { me }, info) => {
+      return me;
+    },
+    messages: () => {
+      return Object.values(messages);
+    },
+    message: (parent, { id }) => {
+      return messages[id];
+    }
   },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id
+      };
+
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+
+      return message;
+    },
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
+    },
+    updateMessage: (parent, { id, text }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return {};
+      }
+
+      message['text'] = text;
+      messages[id] = message;
+
+      return message;
+    }
+  },
+
+  User: {
+    messages: user => {
+      return Object.values(messages).filter(
+        message => message.userId === user.id
+      );
+    }
+  },
+
+  Message: {
+    user: message => {
+      return users[message.userId];
+    }
+  }
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
