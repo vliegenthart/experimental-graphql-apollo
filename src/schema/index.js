811df42f98dd76632df43e57537358b4b107250a
Daniel <daniel.vliegenthart@exotix.com>
2019-05-21 11:48:08 +0100

    Schema stitching + delegated resolvers

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,30 +1,20 @@
 import { gql } from "apollo-server-express";
 
-export default gql`
-  type Query {
-    users: [User!]
-    user(id: ID!): User
-    me: User
+import userSchema from "./user";
+import messageSchema from "./message";
 
-    messages: [Message!]!
-    message(id: ID!): Message!
+const linkSchema = gql`
+  type Query {
+    _: Boolean
   }
 
   type Mutation {
-    createMessage(text: String!): Message!
-    deleteMessage(id: ID!): Boolean!
-    updateMessage(id: ID!, text: String!): Message!
+    _: Boolean
   }
 
-  type User {
-    id: ID!
-    username: String!
-    messages: [Message!]
-  }
-
-  type Message {
-    id: ID!
-    text: String!
-    user: User!
+  type Subscription {
+    _: Boolean
   }
 `;
+
+export default [linkSchema, userSchema, messageSchema];
