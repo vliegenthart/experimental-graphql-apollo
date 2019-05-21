34435dd6d4a2c7e656d92d61d67498674895193e
Daniel <daniel.vliegenthart@exotix.com>
2019-05-21 11:40:58 +0100

    Separation

---
+++ apollo.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  client: {
+    service: "vliegenthart-8202:2Dfk-jXfGENS2O_YN4DVlg"
+  }
+};
---
+++ src/schema/index.js
@@ -0,0 +1,30 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
+  }
+
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
+    updateMessage(id: ID!, text: String!): Message!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
+  }
+`;
