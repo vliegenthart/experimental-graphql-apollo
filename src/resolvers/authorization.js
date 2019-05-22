075373f04f81e45f6373d89f82a811428d301012
Daniel <daniel.vliegenthart@exotix.com>
2019-05-22 11:32:34 +0100

    Add basic role based authorization

--- src/resolvers/authorization.js
+++ src/resolvers/authorization.js
@@ -1,8 +1,14 @@
 import { ForbiddenError } from "apollo-server";
-import { skip } from "graphql-resolvers";
+import { combineResolvers, skip } from "graphql-resolvers";
 
 export const isAuthenticated = (parent, args, { me }) =>
   me ? skip : new ForbiddenError("Not authenticated as user.");
+
+export const isAdmin = combineResolvers(
+  isAuthenticated,
+  (parent, args, { me: { role } }) =>
+    role === "ADMIN" ? skip : new ForbiddenError("Not authorized as admin.")
+);
 
 export const isMessageOwner = async (parent, { id }, { models, me }) => {
   const message = await models.Message.findByPk(id, { raw: true });
